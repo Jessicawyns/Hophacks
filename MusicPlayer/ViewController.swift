912d37d9a14b09426694a342ab62eac8eca315f6
Jessica  NGuyen/ <jnguye45@jhu.edu>
2019-09-14 10:40:53 -0400

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Jessica Nguyen on 9/14/19.
+//  Copyright © 2019 Jessica Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
